1f88a126a13460fead784126d77e846cd7b101ec
Matheus Pacheco <matheus.antunes.pac@gmail.com>
2024-07-06 15:05:16 -0300

    AppBar component code reduction

--- chef-suite-react/src/components/AppBarMenu.tsx
+++ chef-suite-react/src/components/AppBarMenu.tsx
@@ -6,7 +6,6 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
 import { RestaurantMenu } from '@mui/icons-material';
 
 const pages = ['Tables', 'Menu', 'Order', 'Sales'];
@@ -20,7 +19,7 @@ function AppBarMenu() {
 
   return (
     <>
-      <AppBar position="static">
+      <AppBar position="static" color="primary">
         <Container maxWidth="xl">
           <Toolbar disableGutters>
             <RestaurantMenu sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
@@ -42,32 +41,6 @@ function AppBarMenu() {
               Chef Suite
             </Typography>
 
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: 'block', md: 'none' },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
             <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
               {pages.map((page) => (
                 <Button

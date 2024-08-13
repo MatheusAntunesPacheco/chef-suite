6313bace5e778d207e3080c6150bf5e205ce9162
Matheus Pacheco <matheus.antunes.pac@gmail.com>
2024-08-12 22:00:36 -0300

    Configuração do axios para consumir dados da API

--- chef-suite-react/src/services/BffService.js
+++ chef-suite-react/src/services/BffService.js
@@ -7,20 +7,10 @@ const api = axios.create({
 class BffService {
     async getTableList(){
         try{
-            // const response = await api.get('/tables');
-            // return response.data;
-            return [
-                { id: '1', size: '4'},
-                { id: '2', size: '2'},
-                { id: '3', size: '6'},
-                { id: '4', size: '4'},
-                { id: '5', size: '6'},
-                { id: '6', size: '10'},
-                { id: '7', size: '4'},
-                { id: '8', size: '4'},
-                { id: '9', size: '4'},
-                { id: '10', size: '2'}
-            ];
+            
+            const response = await axios.get('/tables');
+            console.log(response.data);
+            return response.data
         }
         catch(error){
             console.error('Erro ao buscar dados da API:', error);
